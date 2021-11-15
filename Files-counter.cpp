1f7999403ab49c87cb17688038126f5c782fd236
Koreczek <P.Skorupka@stud.elka.pw.edu.pl>
2021-11-15 12:32:09 +0100

    initialize

---
+++ Files-counter.cpp
@@ -0,0 +1,12 @@
+﻿// Files-counter.cpp : Defines the entry point for the application.
+//
+
+#include "Files-counter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
